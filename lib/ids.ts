b747fc53db8b9bd1ef0c3ba9485fbdfaef547851
doublestandardsco <dblstandardsco@gmail.com>
2025-08-19 02:39:48 -0700

    Create ids.ts

---
+++ lib/ids.ts
@@ -0,0 +1,7 @@
+// lib/ids.ts
+export function shortId(len = 5) {
+  const chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+  let out = "";
+  for (let i = 0; i < len; i++) out += chars[Math.floor(Math.random() * chars.length)];
+  return out;
+}
